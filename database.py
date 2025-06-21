f4ad6f1d0057d9870664bcbfc4a9e167e3439d80
Vinicius Alexandre <viniciusalexandrerodrigues7@gmail.com>
2025-06-20 22:58:35 -0300

    feat: Adiciona conexão e função para criar tabelas

--- database.py
+++ database.py
@@ -4,10 +4,35 @@ def conectar():
     conn = sqlite3.connect('blog.db')
     return conn
 
-def criar_tabela(conn):
-
+def criar_tabela_usuarios(conn):
     try:
         cursor = conn.cursor()
 
+        cursor.execute("""CREATE TABLE IF NOT EXISTS usuarios (
+            id INTEGER PRIMARY KEY,
+            nome TEXT NOT NULL,
+            email TEXT UNIQUE
+            );
+        """)
+        conn.commit()
+
     except sqlite3.Error as e:
-        print(f"Erro de {e}")
+        print(f"Erro ao criar tabela: {e}")
+
+
+def criar_tabela_posts(conn):
+    try:
+        cursor = conn.cursor()
+
+        cursor.execute("""CREATE TABLE IF NOT EXISTS posts (
+            id INTEGER PRIMARY KEY,
+            titulo TEXT,
+            conteudo TEXT,
+            usuario_id INTEGER,
+            FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+            );
+        """)
+        conn.commit()
+
+    except sqlite3.Error as e:
+        print(f"Erro ao criar tabela: {e}")
